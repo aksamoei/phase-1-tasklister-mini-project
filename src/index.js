fe585580a1b844918f57640ec0d2b354cf899e9c
Abraham <abukip344@gmail.com>
2024-03-29 07:37:37 -0400

    lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let myForm = document.querySelector("#create-task-form");
+  myForm.addEventListener("submit", function (event){
+    event.preventDefault();
+    alert('Hombre you suck big time')
+  })
 });
